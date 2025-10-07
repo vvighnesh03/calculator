f2298cf647009c1b09b0b74535363726a4706220
vvighnesh03 <vichuvighnesh03@gmail.com>
2025-10-07 11:34:59 +0530

    Add calculator files

---
+++ script.js
@@ -0,0 +1,22 @@
+
+let result = document.getElementById('result');
+
+function appendValue(value) {
+    result.value += value;
+}
+
+function clearResult() {
+    result.value = '';
+}
+
+function deleteLast() {
+    result.value = result.value.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        result.value = eval(result.value);
+    } catch (error) {
+        result.value = 'Error';
+    }
+}
